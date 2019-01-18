7c23548897eb7e78e41ad65b696ddfdfc5066a8d
CEWL <you@example.com>
2019-01-18 15:40:10 -0500

    Added updates drive code

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -5,6 +5,8 @@ import frc.robot.Robot;
 import frc.robot.subsystems.DriveTrain;
 
 public class TankDriveCommand extends Command {
+    double currentSpeedLeft = 0;
+	double currentSpeedRight = 0;
     // declare subsystem variable
     DriveTrain driveTrain;
     public TankDriveCommand() {
@@ -13,7 +15,7 @@ public class TankDriveCommand extends Command {
         driveTrain = DriveTrain.getInstance();
         
         //required for each command to know which subsystems it will be using
-        //requires(driveTrain);
+        requires(driveTrain);
     }
 
     @Override
@@ -27,34 +29,59 @@ public class TankDriveCommand extends Command {
 
         // your code goes here:
     double x = Robot.oi.axisLeft_X();
-    double r2 = Robot.oi.axisTrigger_R2();
-    double l2 = Robot.oi.axisTrigger_L2();
-    double forwardSpeed = Math.atan(r2);
-    double backwardSpeed = -Math.atan(l2);
+    double v = Robot.oi.axisTrigger_R2();
+    double w = Robot.oi.axisTrigger_L2();
+    double forwardSpeed = v;
+    double backwardSpeed = -w;
     double leftSpeed = forwardSpeed+backwardSpeed;
     double rightSpeed = forwardSpeed+backwardSpeed;
 
     if (x > 0.1) {
-        rightSpeed= rightSpeed*(1-x);
-    }
-    if(x<-0.1){
-        leftSpeed = leftSpeed*(1+x);
+			rightSpeed = (rightSpeed*(1+x));
+		}
+		else if (x < -0.1) {
+			leftSpeed = (leftSpeed *(1-x));
+		}
+
+		if(0.2> Math.abs(v+w)){
+			//System.out.println("y is between -0.3 and 0.3");
+			if(x<-0.3){
+				//System.out.println("turn left");
+				rightSpeed = 0.5*x;
+				leftSpeed = 0.5*-x;
+			}
+			else if(x>0.3){
+				//System.out.println("turn right");
+				leftSpeed = 0.5*-x;
+				rightSpeed = 0.5*x;
+			}
+		}
+		double finalLeftSpeed = bufferSpeedLeft(this.currentSpeedLeft, leftSpeed);
+		double finalRightSpeed = bufferSpeedLeft(this.currentSpeedRight, rightSpeed);
+		driveTrain.setLeftGroupSpeed(finalLeftSpeed);
+        driveTrain.setRightGroupSpeed(finalRightSpeed);
+		this.currentSpeedLeft = finalLeftSpeed;
+		this.currentSpeedRight = finalRightSpeed;
+	
     }
 
-    if ((Math.abs(l2)-Math.abs(r2))<0.1){
-      if(x>0.1){
-        rightSpeed = -0.5;
-        leftSpeed = 0.5;
-      }
-      if(x<-0.1){
-        rightSpeed = 0.5;
-        leftSpeed = -0.5;
-      }
-    }
-    driveTrain.setLeftGroupSpeed(leftSpeed);
-    driveTrain.setRightGroupSpeed(rightSpeed);
-    }
+    public double bufferSpeedLeft(double currentSpeedLeft, double desiredSpeedLeft){
+		double speedIncrement=desiredSpeedLeft;
+		double threshold = 0.08;
+		if(Math.abs(desiredSpeedLeft-currentSpeedLeft)>threshold){
+			if(desiredSpeedLeft>currentSpeedLeft){
+				speedIncrement=currentSpeedLeft+threshold;
+			}
+			if(desiredSpeedLeft<currentSpeedLeft){
+				speedIncrement=currentSpeedLeft-threshold;
+			}
+		}
+		if(Math.abs(desiredSpeedLeft-currentSpeedLeft)<0.1){
+			speedIncrement=desiredSpeedLeft;
+		}
 
+        return speedIncrement;
+    }
     @Override
     protected boolean isFinished() {
         return false;
